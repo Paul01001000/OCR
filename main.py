ce82cfc1d0fb5b69a313b7c7f8e03a227286b20d
Paul01001000 <max.kraft363@gmail.com>
2025-01-23 21:15:14 +0100

    Write to file

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from os import listdir
 
 # read image
 root = "C:/Users/paul8/Documents/Uni/7. Thesis/Image Processing/OCR/"
-folders = ["English/","German/","Korean/"]
+folders = ["English","German","Korean"]
 lang_list = [['en','de'],['de','en'],['ko','en']]
-cur = 0
+cur = 1
 img_folder = root + folders[cur]
 
 # instance text detector
@@ -17,7 +17,7 @@ reader = easyocr.Reader(lang_list[cur], gpu=False)
 threshold = 0.5
 
 for img_name in listdir(img_folder):
-    img_path = img_folder + img_name
+    img_path = img_folder + "/" + img_name
     print(img_path)
     img = cv2.imread(img_path)
     detected = reader.readtext(img)
@@ -25,10 +25,14 @@ for img_name in listdir(img_folder):
     # draw bbox and text
     for _, t in enumerate(detected):
         print(t)
+        with open(root + folders[cur] + "-out/" + img_name + ".txt", "a", encoding="utf-8") as f:
+            f.write(str(t) + ";\n")
+
         try:
             bbox, text, score = t
         except ValueError:
             continue
+
         if score > threshold:
             pt1,pt2 = list(map(int,bbox[0])),list(map(int,bbox[2]))
 
@@ -38,6 +42,7 @@ for img_name in listdir(img_folder):
 
     plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
     plt.show()
-    cv2.imwrite(img_path + "-out.png",img)
+    cv2.imwrite(root + folders[cur] + "-out/" + img_name + "-out.png", img)
+    break
 
     
